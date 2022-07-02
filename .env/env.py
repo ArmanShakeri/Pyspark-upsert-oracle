cb1f8bad0026edca9efd609c6617590d6210c5e5
Arman <shakeri512@gmail.com>
2022-07-02 16:26:12 +0430

    edit dotenv file

---
+++ .env/env.py
@@ -0,0 +1,11 @@
+input_path="hdfs://172.17.135.31:9000/input/" #path to files on HDFS
+table_name="RESULT_TABLE"
+list_of_keys=["MSISDN","CDR_TYPE"] # key columns of oracle table
+
+##Oracle connection info
+
+host="172.17.135.46"
+port="1521"
+user_name="system"
+password="hadoop"
+sid="orcl"
--- config.py
+++ config.py
@@ -1,11 +1,13 @@
-input_path='hdfs://172.17.135.31:9000/input/' #path to files on HDFS
-table_name="RESULT_TABLE"
-list_of_keys=['MSISDN','CDR_TYPE']
+import os
+from dotenv import load_dotenv
 
-##Oracle connection info
+load_dotenv()
 
-host='172.17.135.46'
-port='1521'
-user_name='system'
-password='hadoop'
-sid='orcl'
+input_path=os.environ.get("input_path","hdfs://172.17.135.31:9000/input/")
+table_name=os.environ.get("table_name","RESULT_TABLE")
+list_of_keys=os.environ.get("list_of_keys",["MSISDN","CDR_TYPE"])
+host=os.environ.get("host","172.17.135.46")
+port=os.environ.get("port","1521")
+user_name=os.environ.get("user_name","system")
+password=os.environ.get("password","hadoop")
+sid=os.environ.get("sid","orcl")
